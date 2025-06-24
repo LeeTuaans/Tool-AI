319d1c987be763a89b3a96894d6343e9f877438b
DangLePhuongNam <2124802010760@student.tdmu.edu.vn>
2025-06-24 14:22:34 +0700

    Revert "Thêm env"
    This reverts commit 85caa27c0edb6315e9d2fca5ae5293d39d8d6151.

--- frontend-toolAI/JavaScript/renderer.js
+++ frontend-toolAI/JavaScript/renderer.js
@@ -1,3 +1,5 @@
+// const apiKey = "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";
+
 // document.getElementById("generate-btn").addEventListener("click", async () => {
 //   const videoContent = document.getElementById("video-title").value.trim();
 //   const checkboxes = document.querySelectorAll('.checkbox-group input[type="checkbox"]:checked');
@@ -67,9 +69,7 @@
 //   }
 // });
 //-----------------------------------------------------------------------------------------------------
-require('dotenv').config(); // Nạp biến môi trường từ .env
-const apiKey = process.env.OPENAI_API_KEY;
-console.log("API Key:", apiKey); // Kiểm tra nếu cần
+const apiKey = "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";
 
 // ====== GẮN SỰ KIỆN CHO CÁC TRANG KHÁC NHAU ======
 window.addEventListener("DOMContentLoaded", () => {
